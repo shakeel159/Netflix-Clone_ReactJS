9006b7fb76c4d38ea2cf8143bb993999455f80fb
shakeel159 <jazibshaq@gmail.com>
2025-03-04 15:48:33 -0600

    typo fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect  } from "react";
-import { BrowserRouter  as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter  as Router, Routes, Route, Navigate  } from "react-router-dom";
 import Sidebar from './Sidebar';
 import MainPage from './MainPage';
 import FavouritePage from './Fav.js';
