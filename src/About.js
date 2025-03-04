7c5ed10afd79987e85ee52515cfa5c5309b310e2
shakeel159 <jazibshaq@gmail.com>
2025-03-04 15:14:23 -0600

    typo edit

--- src/About.js
+++ src/About.js
@@ -5,9 +5,10 @@ function AboutPage(){
     return (
         <div className="TopGap">
            <h1>About Page</h1>
-           <p className="description_About">react.js web application created with TMDB API to allow users to be able to search movies 
-            and obtain certien information about them. this is not a proper functioning site but a personal project
-            showcasing my ability to apply API to an application and ability to programe a functional site.
+           <p className="description_About">react.js web application created with TMDB API to allow users 
+            to be able to search movies and obtain certien information about them. This is not a properly 
+            functioning site, but it is a personal project showcasing my ability to apply API to an 
+            application and my ability to program a functional site.
            </p>
         </div>
     );
